fdab2fa4b32b50b2493ee93e96c85f3a3e9ff823
HardyValen <hardyvalen@gmail.com>
2022-06-28 17:00:08 +0700

    28 Jun 2022

--- routes/debug.js
+++ routes/debug.js
@@ -8,7 +8,7 @@ const { status } = require("../db/config");
 router.get("/storage", (req, res) => {
   minioClient.listBuckets()
   .then((data) => {
-    res.status(200).send(`Connected to Minio, Buckets: ${JSON.stringify(data)}`)
+    res.status(200).send(`Storages: ${JSON.stringify(data)}`)
   })
   .catch((error) => {
     res.status(200).send(`Failed to connect to minio service. Error message: ${error.message}`)
@@ -19,5 +19,8 @@ router.get("/db", (req, res) => {
   res.status(200).send(`Database name: ${status.name}, State: ${status.readyState}`)
 })
 
+router.get("/spec", (req, res) => {
+  res.status(200).json(process.env.SERVER_ID);
+})
 
 module.exports = router;
